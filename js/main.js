edfac085268bd31e82735223e98acfc2a44c80ca
matiaschanquia <matiaschanquianahuel@gmail.com>
2022-08-05 19:28:08 -0300

    añado funcionalidad al nav

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+const botonAbrir = document.getElementById("boton-abrir");
+const botonCerrar = document.getElementById("boton-cerrar");
+const itemsMenu = document.querySelectorAll(".item-menu");
+const nav = document.getElementById("nav-flotante");
+
+
+botonAbrir.addEventListener("click", function() {
+    nav.classList.remove("display-none")
+});
+
+botonCerrar.addEventListener("click", function() {
+    cerrarNav();
+});
+
+itemsMenu.forEach(item => {
+    item.addEventListener("click", function() {
+        cerrarNav();
+    })
+});
+
+function cerrarNav() {
+    nav.classList.add("display-none");
+}
+
+
+
