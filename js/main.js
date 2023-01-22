c0972a9b41945c8354b2370b43a727196cfca8ad
Matias Chanquia <matiaschanquianahuel@gmail.com>
2023-01-21 21:58:11 -0300

    Ajustando nav flotante

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ const botonAbrir = document.getElementById("boton-abrir");
 const botonCerrar = document.getElementById("boton-cerrar");
 const itemsMenu = document.querySelectorAll(".item-menu");
 const nav = document.getElementById("nav-flotante");
+const body = document.body;
 
 
-
-botonAbrir.addEventListener("click", function() {
+botonAbrir.addEventListener("click", function(e) {
+    e.stopPropagation();
     nav.classList.add("active")
 });
 
@@ -16,10 +17,14 @@ botonCerrar.addEventListener("click", function() {
 });
 
 itemsMenu.forEach(item => {
-    item.addEventListener("click", function() {
-        cerrarNav();
-    })
+    item.addEventListener("click", cerrarNav)
 });
+
+nav.addEventListener("click", (e) => {
+    e.stopPropagation();
+})
+
+body.addEventListener("click", cerrarNav)
 
 function cerrarNav() {
     nav.classList.remove("active");
